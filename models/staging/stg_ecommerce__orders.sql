2f9fac3159212557fa0bc0a522b8a0b49d1bdecc
Trieu212203 <ngkhactrieu212@gmail.com>
2026-04-02 14:13:30 +0700

    Adding EDA steps

--- models/staging/stg_ecommerce__orders.sql
+++ models/staging/stg_ecommerce__orders.sql
@@ -15,7 +15,6 @@ renamed as (
         cast(nullif("unit_price", '') as numeric)       as unit_price,
         "customer_id"                                     as customer_id,
         "country"                                        as country,
-        -- derived metric
         (cast(nullif("quantity", '') as numeric) * cast(nullif("unit_price", '') as numeric)) as revenue
     from source
     where "customer_id" is not null and "customer_id" != ''
