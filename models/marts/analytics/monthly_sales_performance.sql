400c86a3e44c36b2364349ce40dbe4b6c8ff069f
Trieu212203 <ngkhactrieu212@gmail.com>
2026-04-16 14:24:02 +0700

    Update monthly_sales_performance.sql

--- models/marts/analytics/monthly_sales_performance.sql
+++ models/marts/analytics/monthly_sales_performance.sql
@@ -1,4 +1,4 @@
--- daily_sales_performance: Daily sales trends
+-- monthly_performance: monthly sales trends
 
 with fact_sales as (
     select * from {{ ref('fact_sales') }}
@@ -7,8 +7,6 @@ with fact_sales as (
 monthly_metrics as (
     select
         date_trunc('month', date_key)::date         as month_key,
-        extract(year from date_key)::int            as year,
-        extract(month from date_key)::int           as month,
         to_char(date_key, 'Mon YYYY')               as month_label,
         count(distinct invoice_no)                  as order_count,
         count(distinct customer_id)                 as customer_count,
@@ -17,7 +15,7 @@ monthly_metrics as (
         round(sum(revenue) /
             nullif(count(distinct invoice_no), 0), 2) as avg_order_value
     from fact_sales
-    group by 1, 2, 3, 4
+    group by 1, 2
 )
 
 select * from monthly_metrics
